45a34dcdcdb08dcb651dc5f5a3484497b834349d
Alexia Myers <103599548+alexiamyers@users.noreply.github.com>
2022-07-20 13:06:23 -0500

    Update transect_inventory .R

--- R/transect_inventory .R
+++ R/transect_inventory .R
@@ -30,6 +30,8 @@
 #' @export
 
 transect_inventory <- function(data_set) {
+  
+  if (!is.data.frame(data_set)) {stop("data_set must be a dataframe obtained from the universalFQA.org website. Type ?download_assessment for help.", call. = FALSE)}
 
   renamed <- data_set %>%
     rename("one" = 1,
